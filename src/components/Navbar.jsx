2dc71dc04593fe1ccc4391eda6d276faed9ea02e
Reena kumari <reenakvs1@gmail.com>
2024-09-10 16:49:15 +0530

    navbar images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { appleImg } from "../utils"
+import { appleImg, bagImg, searchImg } from "../utils"
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+        <nav className="flex w-full screen-max-width">
             <img src={appleImg} alt="Apple" width={14} height={18}/>
             <div>
                 {['Phones', 'Macbooks', 'Tablets'].map((nav,i)=>(
@@ -13,7 +13,8 @@ const Navbar = () => {
                 ))}
 
                 <div>
-                    <img src=""/>
+                    <img src={searchImg} alt="search" width={18} height={18}/>
+                    <img src={bagImg} alt="bag" width={18} height={18}/>
                 </div>
             </div>
         </nav>
