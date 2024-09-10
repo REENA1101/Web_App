da0edc8f07274c84d7f7938d633b87698e2fc180
Reena kumari <reenakvs1@gmail.com>
2024-09-10 20:43:45 +0530

    Navbar solved

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
 
 const Hero = () => {
+    useGSAP(()=>{
+        gsap.to('#hero', {opacity:1})
+    }, [])
+
   return (
     <section className="w-full nav-height bg-black relative">
         <div className="h-5/6 w-full flex-center flex-col">
-           <p>iPhone 15 Pro</p>
+           <p id="hero" className="hero-title">iPhone 15 Pro</p>
         </div>
     </section>
   )
