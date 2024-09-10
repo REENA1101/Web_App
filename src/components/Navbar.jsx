bae1f273b913029e1afbb8847a8441ea6789ef47
Reena kumari <reenakvs1@gmail.com>
2024-09-10 16:51:50 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,8 @@ const Navbar = () => {
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
         <nav className="flex w-full screen-max-width">
             <img src={appleImg} alt="Apple" width={14} height={18}/>
-            <div>
+
+            <div className="flex flex-1 justify-center max-sm:hidden">
                 {['Phones', 'Macbooks', 'Tablets'].map((nav,i)=>(
                     <div key={nav}>
                         {nav}
