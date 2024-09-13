347ebd601defe2eec3d074a9ad0a9ec73351836e
Reena kumari <reenakvs1@gmail.com>
2024-09-13 19:23:36 +0530

    videocarousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -9,7 +9,25 @@ const VideoCarousel = () => {
         {hightlightsSlides.map((list, i)=>(
             <div key={list.id} id="slider" className="sm:pr-20 pr-10">
                 <div className="video-carousel_container">
-                    <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black"></div>
+                    <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                        <video
+                        id="video"
+                        playsInline={true}
+                        preload="auto"
+                        muted
+                        >
+                            <source src={list.video} type="video/mp4"/>
+                        </video>
+                    </div>
+
+                    <div className="absolute top-12 left-[5%] z-10">
+                        {list.textLists.map((text)=>(
+                            <p key={text} className="md:text-2xl text-xl font-medium">
+                                {text}
+                            </p>
+                        ))}
+
+                    </div>
                 </div>
             </div>
         ))}
