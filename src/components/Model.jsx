491b4bb49d7e46c31be7a883f5d71df7a9af97f2
Reena kumari <reenakvs1@gmail.com>
2024-09-19 09:56:20 +0530

    done

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 export default function Model() {
+   useGSAP(()=>{
+    gsap.to('#heading', {y:0, opacity:1})
+   }, []);
+
+
   return (
-    <div>Model</div>
+    <section className='common-padding'>
+        <div className='screen-max-width'> 
+            <h1 id="heading" className='section-heading'>
+              Take a closer look.
+            </h1>
+        </div>
+
+    </section>
   )
 }
