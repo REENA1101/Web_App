d6b817223ab4110a7c05a822edfc1f5466d25933
Reena kumari <reenakvs1@gmail.com>
2024-09-30 07:46:23 +0530

    done

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,14 +1,15 @@
 import { PerspectiveCamera, View } from "@react-three/drei"
 import Lights from "./Lights"
 import { Suspense } from "react"
+import IPhone from "./IPhone"
 
 export default function ModelView({index, groupRef, gsapType, controlRef,setRotationSize, size, item }) {
   return (
     <div>
       <View
-        index = {index}
+        index={index}
         id={gsapType}
-        className={`border-2 border-red-500 w-full h-full ${index===2} ? 'right-[-100%] :''`}
+        className = {`border-2 border-red-500 w-full h-full ${index===2} ? 'right-[-100%] :''`}
     >
       {/* Ambient Light */}
       <ambientLight intensity = {0.3}/>
@@ -18,13 +19,10 @@ export default function ModelView({index, groupRef, gsapType, controlRef,setRota
       <Lights/>
 
       <Suspense fallback={<div>Loading</div>}>
-      
+      <IPhone/>
 
       </Suspense>
      </View>
     </div>
   )
 }
-
-{/* <h1></h1> */}
-{/* <h1></h1> */}
